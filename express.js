7bd3adbef5f884334c095597dd6edbe881ade52f
yashas <yashu.gsb@gmail.com>
2019-01-14 11:44:36 +0530

    initalize

---
+++ express.js
@@ -0,0 +1,30 @@
+var express = require('express');
+
+var auto =express();
+
+var middle={
+    
+    requireAuthentication : function(req, res,next){
+        console.log("private!!!");
+        next();
+    },
+    
+    logger: function(req,res,next){
+        console.log("Request: " +Date()+" "+req.method+ ""+req.originalUrl);
+            next();
+        
+    }
+    
+}
+
+auto.use(middle.logger);
+
+auto.get('/ome',middle.requireAuthentication,function(req,res){
+    
+    res.send("yashs");
+    
+} );
+
+auto.use(express.static(__dirname+"/public"));
+console.log(__dirname);
+auto.listen(3000);
